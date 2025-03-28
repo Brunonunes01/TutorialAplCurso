1ec15921324d53450fa7c1baa26e6a6283ad783f
Brunonunes01 <brunohenriqueboraschonunes@hotmail.com>
2025-03-28 10:07:14 -0300

    commit inicial

---
+++ build/web/menu.jsp
@@ -0,0 +1,9 @@
+<h1>Módulo Cadastro</h1>
+<hr>
+      <center>
+         <h2>Menu Principal</h2>
+         <a href="${pageContext.request.contextPath}/EstadoListar">Estado</a>
+         <a href="${pageContext.request.contextPath}/CidadeListar">Cidade</a>
+      </center>
+<hr>
+   
---
+++ src/java/br/com/curso/dao/EstadoDAO.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.curso.dao;
+
+import br.com.curso.model.Estado;
+import br.com.curso.utils.SingleConnection;
+import java.sql.Connection;
+import java.util.List;
+import java.util.ArrayList;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+
+
+/**
+ *
+ * @author Bruno
+ */
+public class EstadoDAO implements GenericDAO{
+    
+    private Connection conexao;
+    
+    public EstadoDAO() throws Exception{
+       conexao = SingleConnection.getConnection();
+    }
+
+    @Override
+    public Boolean cadastrar(Object objeto) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Boolean inserir(Object objeto) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Boolean alterar(Object objeto) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Boolean excluir(int numero) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object carregar(int numero) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<Object> listar() {
+        List<Object> resultado = new ArrayList<>();
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        String sql = "Select * from estado order by idEstado";
+        try {
+            stmt = conexao.prepareStatement(sql);
+            rs=stmt.executeQuery();
+            while (rs.next()) {
+                Estado oEstado = new Estado();
+                oEstado.setIdEstado(rs.getInt("idEstado"));
+                oEstado.setNomeEstado(rs.getString("nomeestado"));
+                oEstado.setSiglaEstado(rs.getString("siglaestado"));
+                resultado.add(oEstado);
+            }
+        } catch (SQLException ex) {
+            System.out.println("Problemas ao listar Estado! Erro: "
+                    +ex.getMessage());
+        }
+        return resultado;
+    }
+    }
+    
+
